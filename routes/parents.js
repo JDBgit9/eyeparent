63fdf261c19f41fc54c66db629925f0132330bad
JDBgit9 <jamesthejust99@outlook.com>
2022-05-27 15:27:55 -0400

    router.get, post, pass

--- routes/parents.js
+++ routes/parents.js
@@ -1,5 +1,27 @@
 const express = require('express')
 const router = express.Router()
 
+//Getting all
+router.get('/', (req, res) => {
+
+})
+
+//Getting one 
+ router.get('/:id', (req, res) => {
+    
+}) 
+
+//Creating One
+router.post('/', (req, res) => {
+    
+})
+
+//Updating One
+
+
+//Deleting one
+
+
+
 
 module.exports = router
