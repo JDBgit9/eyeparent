fe68cd7550cc93c3e25f21c26015fc73722f3228
JDBgit9 <jamesthejust99@outlook.com>
2022-05-27 17:35:07 -0400

    added more items in the schema

--- models/parents.js
+++ models/parents.js
@@ -1,57 +1,93 @@
+const { Timestamp } = require('mongodb')
 const mongoose = require('mongoose')
 
 
 const parentsSchema = new mongoose.Schema({
     name: {
+        type: String,
+        required: true
 
     },
     nameOfChild: {
+        type: String,
+        required: true
 
     },
     busNumber: {
+        type: Number,
+        required: true
 
     },
     nameOfSchool: {
+        type: String,
+        required: true
 
     },
     nameOfCounty: {
+        type: String,
+        required: true
 
     },
     classroomOne: {
+        type: String,
+        required: true
 
     },
     classroomTwo: {
+        type: String,
+        required: true
 
     },
     classroomThree: {
+        type: String,
+        required: true
 
     },
     classroomFour: {
+        type: String,
+        required: true
 
     },
     classroomFive: {
+        type: String,
+        required: true
 
     },
     classroomSix: {
+        type: String,
+        required: true
 
     },
     classroomSeven: {
+        type: String,
+        required: true
 
     },
     LunchRoom: {
+        type: Timestamp,
+        required: true,
+        default: Timestamp.now
 
     },
 
     gymRoom: {
+        type: String,
+        required: true
 
     },
     adminOffice: {
+        type: String,
+        required: true
 
     },
     theatreRoom:  {
+        type: String,
+        required: true
 
     },
     playGround: {
+        type: String,
+        required: true
 
     }
 
