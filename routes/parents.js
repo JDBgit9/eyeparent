ddc5f9ed10cb2b36e28e3a99b3a57a68f1ac6b4c
JDBgit9 <jamesthejust99@outlook.com>
2022-05-30 10:39:42 -0400

    corretcted the middleware

--- routes/parents.js
+++ routes/parents.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const Parent = require('../models/parents')
 
 //Getting all
-router.get('/', async (req, res) => {
+router.get('/', getParent, async (req, res) => {
 try {
      const parents = await Parent.find()
      res.json(res.parent)
@@ -14,7 +14,7 @@ try {
 })
 
 //Getting one 
- router.get('/:id', getParent, async (req, res) => {
+ router.get('/', getParent, (req, res) => {
   res.json(res.parent)
 }) 
 
@@ -28,23 +28,32 @@ router.post('/', getParent, async (req, res) => {
       const newParent = await parent.save() 
        res.status(201).json(newParent)
     } catch (err){
-       res.status(400).json({message: err.message})
+       res.status(400).json({ message: err.message })
     }
 })
 
 //Updating One
 router.patch('/:id', getParent, async (req, res) => {
-    
+    if (req.body.parent != null) {
+      res.parent.name = req.body.name
+   }
+   if (req.body.childName != null) {
+    res.parent.childName = req.body.childName
+   }
+   try{
+      const updatedParent = await res.parent.save()
+      res.json(updatedParent)
+   } catch (err) {
+       res.status(400).json({ message: err.message })
+   }
 })
-
-
 //Deleting one
 router.delete('/:id', getParent, async (req, res) => {
  try{
      await res.parent.remove()
      res.json('Deleted Parent')
  }catch (err) { 
-   res.status(500).json({message: err.message})
+   res.status(500).json({ message: err.message })
  }
 })
 
@@ -53,7 +62,7 @@ async function getParent(req, res, next) {
   try {
      parent = await Parent.findById(req.params.id)
      if (parent == null) {
-       return res.status(404).json({message: 'Cannot find Parent'})
+       return res.status(404).json({ message: 'Cannot find Parent' })
      }
   } catch (err) {
     return res.status(500).json({ message: err.message })
